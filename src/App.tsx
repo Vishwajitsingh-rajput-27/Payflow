028ce83a4801dea9a420d243919d412fa1b6f1e6
Vishwajitsingh rajput <vishwajitrajput1238@gmail.com>
2026-02-26 13:23:38 +0530

    Remove DeployGuide import and switch case for deploy-guide

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import './App.css';
 
 function App() {
-  const [page, setPage] = React.useState('home');
-
-  switch (page) {
-    case 'home':
-      return <Home onNavigate={setPage} />;
-    case 'admin':
-      return <Admin onNavigate={setPage} />;
-    case 'user':
-      return <User onNavigate={setPage} />;
-    // Removed deploy-guide case
-    default:
-      return <Home onNavigate={setPage} />;
-  }
+  return (
+    <div>
+      {/* Other components */}
+    </div>
+  );
 }
 
 export default App;
